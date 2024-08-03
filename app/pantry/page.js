9448a0f9e9522a6576aece02b14fe89c67844e12
meghagl17 <megha.agl17@gmail.com>
2024-08-03 15:24:35 -0700

    type in updated quantity and search field

--- app/pantry/page.js
+++ app/pantry/page.js
@@ -2,7 +2,10 @@
 import React from 'react';
 
 import { auth, db } from '../../firebase';
+import { collection, addDoc, getDocs, query, updateDoc, getDoc, doc, deleteDoc } from 'firebase/firestore';
+
 import { useState, useEffect } from 'react';
+
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -15,24 +18,26 @@ import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
-
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
-
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 
-import { collection, addDoc, getDocs, query, updateDoc, getDoc, doc, deleteDoc } from 'firebase/firestore';
-
 export default function Home() {
-    const [pantry, setPantry] = useState([]);
-    const [itemName, setItemName] = useState('');
-    const [itemQuantity, setItemQuantity] = useState('');
-    const [loading, setLoading] = useState(true);
-    const [user, setUser] = useState('');
-    const [itemDelete, setItemDelete] = useState({name: '', id: ''});
+    const [pantry, setPantry] = useState([]);     // all the items in the pantry
+    const [itemName, setItemName] = useState('');     // new item to add
+    const [itemQuantity, setItemQuantity] = useState('');    // new item quantity
+    const [loading, setLoading] = useState(true);     // loading state
+    const [user, setUser] = useState('');       // current user
+    const [itemDelete, setItemDelete] = useState({name: '', id: ''});    // item thats quantity is now 0
+    const [itemToUpdate, setItemToUpdate] = useState({id: '', name: ''}); // item thats quantity is to be updated
+    const [updateQuantity, setUpdatedQuantity] = useState(''); // quantity of the updated item
 
+    const [searchQuery, setSearchQuery] = useState(''); // stores what the user searched for
+    const [filteredPantry, setFilteredPantry] = useState([]);  // stores the filtered list
+
+    // Initially get the user
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((user) => {
             if (user) {
@@ -46,57 +51,46 @@ export default function Home() {
         return () => unsubscribe();
     }, []);
 
+    // initiallt get the items once the user is loaded
     useEffect(() => {
         if (user) {
             handleGetItems();
         }
     }, [user]);
 
-    const updateInventory = async (itemName, itemQuantity) => {
-        if (!user) return; 
+    useEffect(() => {
+        filterPantryItems();
+    }, [searchQuery, pantry]);
+
+    const filterPantryItems = () => {
+        const filtered = pantry.filter(item =>
+            item.name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+        setFilteredPantry(filtered);
+    };
+
+    // const handleSearchChange = (event) => {
+    //     setSearchQuery(event.target.value);
+    // };
+
+    // Get all the items in the pantry for the user
+    const handleGetItems = async () => {
+        if (!user) return;
         const userId = user.uid;
         const pantryCollectionRef = collection(db, 'users', userId, 'pantry');
-        await addDoc(pantryCollectionRef, {
-            name: itemName,
-            addedAt: new Date(),
-            quantity: itemQuantity,
-        });
-        console.log(`Item ${itemName} added to pantry`);
-    }
+        const q = query(pantryCollectionRef);
+        const querySnapshot = await getDocs(q);
 
-    const addToInventory = async (itemId) => {
-        if (!user) return; 
-        const userId = user.uid;
-        const itemDocRef = doc(db, 'users', userId, 'pantry', itemId);
-        const itemDoc = await getDoc(itemDocRef);
-        const currentQuantity = itemDoc.data().quantity;
-        await updateDoc(itemDocRef, {
-            quantity: currentQuantity + 1
-        });
-        handleGetItems();
-        console.log(`Item ${itemId} added to pantry`);
-    }
+        const items = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+        }));
 
-    const removeFromInventory = async (itemId, itemName) => {
-        if (!user) return; 
-        const userId = user.uid;
-        const itemDocRef = doc(db, 'users', userId, 'pantry', itemId);
-        const itemDoc = await getDoc(itemDocRef);
-        const currentQuantity = itemDoc.data().quantity;
-        if(currentQuantity - 1 == 0){
-            setItemDelete({name: itemName, id: itemId})
-            handleClickOpen();
-            // open pop up
-            // if user says yes, add it to user shopping cart and delete from pantry
-            // if user says no, delete it from shopping cart
-        }
-        await updateDoc(itemDocRef, {
-            quantity: currentQuantity - 1
-        });
-        handleGetItems();
-        console.log(`Item ${itemId} removed from pantry`);
-    }
+        setPantry(items);
+        console.log("Pantry items:", items);
+    };
 
+    // Adding a new item to the inventory
     const handleAddItem = async () => {
         if (itemName.trim() !== '' && itemQuantity.trim() !== '') {
             const quantity = parseFloat(itemQuantity);
@@ -114,28 +108,109 @@ export default function Home() {
         }
     };
 
-    const handleGetItems = async () => {
-        if (!user) return;
+    // Helper funtion to update the inventory with a new item
+    const updateInventory = async (itemName, itemQuantity) => {
+        if (!user) return; 
         const userId = user.uid;
         const pantryCollectionRef = collection(db, 'users', userId, 'pantry');
-        const q = query(pantryCollectionRef);
-        const querySnapshot = await getDocs(q);
+        await addDoc(pantryCollectionRef, {
+            name: itemName,
+            addedAt: new Date(),
+            quantity: itemQuantity,
+        });
+        console.log(`Item ${itemName} added to pantry`);
+    }
 
-        const items = querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data()
-        }));
+    // Add +1 to an item
+    const addToInventory = async (itemId) => {
+        if (!user) return; 
+        const userId = user.uid;
+        const itemDocRef = doc(db, 'users', userId, 'pantry', itemId);
+        const itemDoc = await getDoc(itemDocRef);
+        const currentQuantity = itemDoc.data().quantity;
+        await updateDoc(itemDocRef, {
+            quantity: currentQuantity + 1
+        });
+        handleGetItems();
+        console.log(`Item ${itemId} added to pantry`);
+    }
 
-        setPantry(items);
-        console.log("Pantry items:", items);
+    // Subtract -1 to an item
+    const removeFromInventory = async (itemId, itemName) => {
+        if (!user) return; 
+        const userId = user.uid;
+        const itemDocRef = doc(db, 'users', userId, 'pantry', itemId);
+        const itemDoc = await getDoc(itemDocRef);
+        const currentQuantity = itemDoc.data().quantity;
+        if(currentQuantity - 1 == 0){
+            setItemDelete({name: itemName, id: itemId})
+            handleClickOpen();
+        }
+        await updateDoc(itemDocRef, {
+            quantity: currentQuantity - 1
+        });
+        handleGetItems();
+        console.log(`Item ${itemId} removed from pantry`);
+    }
+
+    // dialog for when the user wants to update the quantity of an item
+    // state for dialog
+    const [openForUpdate, setOpenForUpdate] = useState(false);
+
+    // open function for dialog
+    const handleClickOpenForUpdate = (itemId, itemName) => {
+        setItemToUpdate({id: itemId, name: itemName});
+        setOpenForUpdate(true);
+    };
+
+    // close function for dialog which will also add to shopping cart and delete from pantry
+    const handleCloseForUpdate = async (updateItem) => {
+        if (!user) return; 
+        const userId = user.uid;
+
+        if (updateQuantity.trim() !== '') {
+            const quantity = parseFloat(updateQuantity);
+            if (isNaN(quantity)) {
+                alert('Quantity must be a valid number');
+                return;
+            }
+
+            if(updateItem){
+                if(quantity == 0){
+                    setItemDelete({name: itemToUpdate.name, id: itemToUpdate.id});
+                    setUpdatedQuantity('');
+                    setItemToUpdate({id: '', name: ''})
+                    setOpenForUpdate(false);
+                    handleClickOpen();
+                }
+
+                const itemDocRef = doc(db, 'users', userId, 'pantry', itemToUpdate.id);
+                const itemDoc = await getDoc(itemDocRef);
+                // const currentQuantity = itemDoc.data().quantity;
+                await updateDoc(itemDocRef, {
+                    quantity: quantity
+                });
+                // handleGetItems();
+                // console.log(`Item ${itemId} added to pantry`);
+                handleGetItems();
+            }
+
+            setUpdatedQuantity('');
+            setItemToUpdate({id: '', name: ''})
+        }
+        setOpenForUpdate(false);
     };
     
+    // dialog for when the user clicks "-" and the item quantity becomes 0
+    // state for dialog
     const [open, setOpen] = useState(false);
 
+    // open function for dialog
     const handleClickOpen = () => {
         setOpen(true);
     };
 
+    // close function for dialog which will also add to shopping cart and delete from pantry
     const handleClose = async (itemDelete, addtoShoppingCart) => {
         console.log(itemDelete);
         if (!user) return; 
@@ -162,31 +237,55 @@ export default function Home() {
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px' }}>
-            {/* {itemFinish ? (<div> */}
-                <React.Fragment>
-                    <Dialog
-                        open={open}
-                        aria-labelledby="alert-dialog-title"
-                        aria-describedby="alert-dialog-description"
-                    >
-                        <DialogTitle id="alert-dialog-title">
-                        {"Shopping List Update"}
-                        </DialogTitle>
-                        <DialogContent>
-                        <DialogContentText id="alert-dialog-description">
-                            Since this item is not in your pantry anymore, do you want to add it to your shopping list?
-                        </DialogContentText>
-                        </DialogContent>
-                        <DialogActions>
-                        <Button onClick={() => handleClose(itemDelete, false)}>NO</Button>
-                        <Button onClick={() => handleClose(itemDelete, true)} autoFocus>
-                            YES
-                        </Button>
-                        </DialogActions>
-                    </Dialog>
-                </React.Fragment>
-                {/* </div>)  */}
-             {/* : (null)} */}
+            <React.Fragment>
+                <Dialog
+                    open={open}
+                >
+                    <DialogTitle id="alert-dialog-title">
+                    {"Shopping List Update"}
+                    </DialogTitle>
+                    <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Since this item is not in your pantry anymore, do you want to add it to your shopping list?
+                    </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={() => handleClose(itemDelete, false)}>NO</Button>
+                    <Button onClick={() => handleClose(itemDelete, true)} autoFocus>
+                        YES
+                    </Button>
+                    </DialogActions>
+                </Dialog>
+            </React.Fragment>
+
+            <React.Fragment>
+                <Dialog open={openForUpdate}>
+                    <DialogTitle id="alert-dialog-title">
+                        Update {itemToUpdate.name} Quantity
+                    </DialogTitle>
+                    <DialogContent>
+                    {/* <DialogContentText id="alert-dialog-description">
+                        Item: {itemToUpdate.name}
+                    </DialogContentText> */}
+                    <TextField
+                        style={{ width: '150px' }}
+                        id="item-quantity"
+                        label="Quantity"
+                        variant="outlined"
+                        value={updateQuantity}
+                        onChange={(e) => setUpdatedQuantity(e.target.value)}
+                        placeholder="Quantity"
+                    />
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={() => handleCloseForUpdate(false)}>Cancel</Button>
+                    <Button onClick={() => handleCloseForUpdate(true)} autoFocus>
+                        Update
+                    </Button>
+                    </DialogActions>
+                </Dialog>
+            </React.Fragment>
+
             <div style={{ display: 'flex', alignItems: 'center', gap: '10px', marginBottom: '20px', width: '100%', maxWidth: '600px' }}>
                 <TextField
                     fullWidth
@@ -211,6 +310,16 @@ export default function Home() {
                 </Button>
             </div>
 
+            <TextField
+                fullWidth
+                label="Search your pantry"
+                variant="outlined"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                placeholder="Search items..."
+                style={{ marginBottom: '20px' }}
+            />
+
             <Box
                 sx={{
                     display: 'grid',
@@ -227,28 +336,51 @@ export default function Home() {
                     }
                 }}
             >
-                {pantry.map((item) => (
-                    <Card key={item.id} sx={{ minWidth: 275, maxWidth: 400 }}>
-                        <CardContent>
-                            <Typography variant="h5" component="div">
-                                {item.name}
-                            </Typography>
-                            <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-                                {item.quantity}
-                            </Typography>
-                        </CardContent>
-                        <Divider />
-                        <Box sx={{ p: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                            <Typography gutterBottom variant="body2">
-                                Update Item
-                            </Typography>
-                            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                <RemoveIcon onClick={() => removeFromInventory(item.id, item.name)} style={{ cursor: 'pointer' }} />
-                                <AddIcon onClick={() => addToInventory(item.id)} style={{ cursor: 'pointer', marginRight: '8px' }} />
+                {filteredPantry.length === 0 ?
+                    (pantry.map((item) => (
+                        <Card key={item.id} sx={{ minWidth: 275, maxWidth: 400 }}>
+                            <CardContent>
+                                <Typography variant="h5" component="div">
+                                    {item.name}
+                                </Typography>
+                                <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                                    {item.quantity}
+                                </Typography>
+                            </CardContent>
+                            <Divider />
+                            <Box sx={{ p: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                {/* <Typography gutterBottom variant="body2"> */}
+                                <Button onClick = {() => handleClickOpenForUpdate(item.id, item.name)}>Update Item</Button>
+                                {/* </Typography> */}
+                                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                    <RemoveIcon onClick={() => removeFromInventory(item.id, item.name)} style={{ cursor: 'pointer' }} />
+                                    <AddIcon onClick={() => addToInventory(item.id)} style={{ cursor: 'pointer', marginRight: '8px' }} />
+                                </Box>
                             </Box>
-                        </Box>
-                    </Card>
-                ))}
+                        </Card>
+                    ))) : 
+                    (filteredPantry.map((item) => (
+                        <Card key={item.id} sx={{ minWidth: 275, maxWidth: 400 }}>
+                            <CardContent>
+                                <Typography variant="h5" component="div">
+                                    {item.name}
+                                </Typography>
+                                <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                                    {item.quantity}
+                                </Typography>
+                            </CardContent>
+                            <Divider />
+                            <Box sx={{ p: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                {/* <Typography gutterBottom variant="body2"> */}
+                                <Button onClick = {() => handleClickOpenForUpdate(item.id, item.name)}>Update Item</Button>
+                                {/* </Typography> */}
+                                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                    <RemoveIcon onClick={() => removeFromInventory(item.id, item.name)} style={{ cursor: 'pointer' }} />
+                                    <AddIcon onClick={() => addToInventory(item.id)} style={{ cursor: 'pointer', marginRight: '8px' }} />
+                                </Box>
+                            </Box>
+                        </Card>
+                    )))}
             </Box>
         </div>
     );
